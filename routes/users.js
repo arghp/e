a8c4b03c1acd91abd489f99253a51bfc0fb047bf
arghp <arpangurung@hotmail.com>
2019-09-22 03:27:22 -0500

    Added authorization route

--- routes/users.js
+++ routes/users.js
@@ -119,4 +119,57 @@ router.post('/',
 	}
 );
 
+// @route  POST /users/authorize
+// @desc   Authenticate user & get token
+// @access Public
+router.post('/authorize', 
+	[
+	check('email', 'Please include a valid email').isEmail(),
+	check('password', 'Password is required').exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [ { msg: 'Invalid Credentials' }]});
+			}
+
+			const passMatch = await bcrypt.compare(password, user.password);
+			if (!passMatch) {
+				return res
+					.status(400)
+					.json({ errors: [ { msg: 'Invalid Credentials' }]});
+			}
+			
+			const payload = {
+				user: {
+					id: user.id,
+					role: user.role
+				}
+			}
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				});
+
+		} catch(err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
 module.exports = router;
