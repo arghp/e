34c8aa125cb9f034b7588f545ce3a8509437f76b
arghp <arpangurung@hotmail.com>
2019-09-22 11:09:58 -0500

    Added PUT route

--- routes/products.js
+++ routes/products.js
@@ -51,7 +51,7 @@ router.get('/:id', async (req, res) => {
 
 // @route  POST /products
 // @desc   Create a new product
-// @access Public
+// @access Private, admin
 router.post('/', 
 	[
 		authorize(Roles.Admin),
@@ -81,6 +81,48 @@ router.post('/',
 		} catch(err) {
 			console.error(err.message);
 			res.status(500).send('Server Error');
+		}
+	}
+);
+
+// @route  PUT /products/:id
+// @desc   Update a product
+// @access Private, admin
+router.put('/', 
+	[
+		authorize(Roles.Admin),
+		[
+		check('name', 'Name is required').not().isEmpty(),
+		check('description', 'Description is required').not().isEmpty(),
+		check('quantity', 'Quantity is required').not().isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const product = await Product.findById(req.params.id);
+			if (!product) {
+				return res.status(404).json({ msg: 'Product not found'});
+			}
+
+			product.name = req.body.name;
+			product.description = req.body.description;
+			product.quantity = req.body.quantity;
+
+			await product.save()
+			res.json(product);
+		} catch (err) {
+			console.error(err.message);
+
+			if (err.kind === 'ObjectId') {
+				return res.status(404).json({ msg: 'Product not found'});
+			}
+
+			res.status(500).send('Server error');
 		}
 	}
 );
