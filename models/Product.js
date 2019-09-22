01d2b141ed9d31ff2dec389e8422b7e45771a089
arghp <arpangurung@hotmail.com>
2019-09-21 23:30:12 -0500

    Created Product schema

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	quantity: {
+		type: Number,
+		min: 0,
+		max: 999
+	}
+});
+
+module.exports = Product = mongoose.model('products', ProductSchema);
