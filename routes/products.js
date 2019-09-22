93e5d974261bcfd318f8e802f3d82e501636550e
arghp <arpangurung@hotmail.com>
2019-09-22 04:33:05 -0500

    Added GET routes

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
+const authorize = require('../middleware/authorize');
+const Roles = require('../config/roles');
+const Product = require('../models/Product');
 
 // get all
 // get by id
 // post new product
 // update a product
 // delete a product
+
+// @route  GET /products
+// @desc   Get all products
+// @access Public
+router.get('/', async (req, res) => {
+	try {
+		const products = await Product.find().sort({ date: -1});
+		res.json(products);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
+// @route  GET /products/:id
+// @desc   Get product by id
+// @access Public
+router.get('/:id', async (req, res) => {
+	try {
+
+		const product = await Product.findById(req.params.id);
+
+		if (!product) {
+			return res.status(404).json({ msg: 'Product not found'});
+		}
+
+		res.json(product);
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Product not found'});
+		}
+
+		res.status(500).send('Server error');
+	}
+});
